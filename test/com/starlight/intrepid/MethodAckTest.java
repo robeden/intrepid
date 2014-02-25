d586cac0e0ef0e5962eb86df7302fa34af23ab2e
Rob Eden <rob@starlight-systems.com>
2014-02-25 11:10:09 -0600

    Additional unit test to ensure ack isn't sent with fast returning method calls

--- test/com/starlight/intrepid/MethodAckTest.java
+++ test/com/starlight/intrepid/MethodAckTest.java
@@ -1,5 +1,6 @@
 package com.starlight.intrepid;
 
+import com.starlight.intrepid.auth.UserContextInfo;
 import com.starlight.intrepid.exception.MethodInvocationFailedException;
 import com.starlight.intrepid.message.IMessage;
 import com.starlight.intrepid.message.InvokeAckIMessage;
@@ -9,6 +10,7 @@ import com.starlight.thread.ThreadKit;
 import junit.framework.TestCase;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -217,5 +219,100 @@ public class MethodAckTest extends TestCase {
 
 		assertTrue( "Duration < 5000 or > 6000: " + duration,
 			duration >= 5000 && duration < 6000 );
+	}
+
+
+	// Test a fast-returning method call to ensure no ack message is sent.
+	public void testFastReturn() throws Exception {
+		IntrepidTesting.setInterInstanceBridgeDisabled( true );
+
+		final AtomicBoolean sent_ack = new AtomicBoolean( false );
+		PerformanceListener perf_listener = new PerformanceListener() {
+			@Override
+			public void messageSent( VMID destination_vmid, IMessage message ) {
+				if ( message instanceof InvokeAckIMessage ) {
+					sent_ack.set( true );
+				}
+			}
+
+			@Override
+			public void remoteCallStarted( VMID instance_vmid, long time, int call_id,
+				VMID destination_vmid, int object_id, int method_id, Method method,
+				Object[] args, UserContextInfo user_context, String persistent_name ) {}
+
+			@Override
+			public void remoteCallCompleted( VMID instance_vmid, long time, int call_id,
+				Object result, boolean result_was_thrown, Long server_time ) {}
+
+			@Override
+			public void inboundRemoteCallStarted( VMID instance_vmid, long time,
+				int call_id, VMID source_vmid, int object_id, int method_id,
+				Method method, Object[] args, UserContextInfo user_context,
+				String persistent_name ) {}
+
+			@Override
+			public void inboundRemoteCallCompleted( VMID instance_vmid, long time,
+				int call_id, Object result, boolean result_was_thrown ) {}
+
+			@Override
+			public void virtualChannelOpened( VMID instance_vmid, VMID peer_vmid,
+				short channel_id ) {}
+
+			@Override
+			public void virtualChannelClosed( VMID instance_vmid, VMID peer_vmid,
+				short channel_id ) {}
+
+			@Override
+			public void virtualChannelDataReceived( VMID instance_vmid, VMID peer_vmid,
+				short channel_id, int bytes ) {}
+
+			@Override
+			public void virtualChannelDataSent( VMID instance_vmid, VMID peer_vmid,
+				short channel_id, int bytes ) {}
+
+			@Override
+			public void messageReceived( VMID source_vmid, IMessage message ) {}
+
+			@Override
+			public void leaseInfoUpdated( VMID vmid, int object_id,
+				String delegate_tostring, boolean holding_strong_reference,
+				int leasing_vm_count, boolean renew, boolean release ) {}
+
+			@Override
+			public void leasedObjectRemoved( VMID vmid, int object_id ) {}
+		};
+
+
+		server_instance = Intrepid.create(
+			new IntrepidSetup().vmidHint( "server" ).openServer().performanceListener(
+			perf_listener ) );
+
+		Runnable server_impl = new Runnable() {
+			@Override
+			public void run() {
+				// Return immediately
+			}
+		};
+		server_instance.getLocalRegistry().bind( "server", server_impl );
+
+		client_instance = Intrepid.create( new IntrepidSetup().vmidHint( "client" ) );
+
+
+		VMID server_vmid = client_instance.connect( InetAddress.getByName( "127.0.0.1" ),
+			server_instance.getServerPort().intValue(), null, null );
+		Runnable proxy = ( Runnable ) client_instance.getRemoteRegistry(
+			server_vmid ).lookup( "server" );
+
+
+		long start = System.currentTimeMillis();
+
+		proxy.run();
+
+		long duration = System.currentTimeMillis() - start;
+
+		assertTrue( "Duration > 1000: " + duration, duration < 1000 );
+		ThreadKit.sleep( 1, TimeUnit.SECONDS );    // give time for the message to be sent
+
+		assertEquals( false, sent_ack.get() );
 	}
 }
