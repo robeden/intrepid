ba37524aeb4c16c07d604b0467fd86c6814e850a
Rob Eden <rob.eden@forcepointgov.com>
2024-01-22 16:48:49 -0600

    Disable the BOM for now since it's failing publication

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,6 @@ rootProject.name = "intrepid-parent"
 includeBuild("build-support")
 
 include(":intrepid")
-include(":intrepid-bom")
+//include(":intrepid-bom")
 include(":intrepid-driver-mina")
 include(":intrepid-driver-netty")
