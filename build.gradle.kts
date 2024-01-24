4b5565985bed03d4220aa563cb3871ecdb7056da
Rob Eden <rob.eden@forcepointgov.com>
2024-01-23 21:39:56 -0600

    versioning plugin update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    id("net.nemerosa.versioning") version "2.8.2"
+    id("net.nemerosa.versioning") version "3.0.0"
 }
 
 version = versioning.info.full
+println("Version: $version")
 
 allprojects {
     version = rootProject.version
